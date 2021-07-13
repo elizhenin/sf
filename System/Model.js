80bc2266d339f8fa6d339501122dee06f43f278d
elizhenin <elizhenin@gmail.com>
2021-07-13 11:59:48 +0300

    Mail as separate class

--- System/Model.js
+++ System/Model.js
@@ -1,13 +1,11 @@
 //basic Sukina Framework model
 module.exports = class {
-    constructor(db_config = 'default', mailer_config = 'default') {
+    constructor(db_config = 'default') {
         this._settings = {
-            'db_config': db_config,
-            'mailer_config': mailer_config
+            'db_config': db_config
         };
         this.DB = function() {
             return Application.DB[this._settings.db_config];
         };
-        this.Mailer = Application.System.Mail(mailer_config);
     }
 }
