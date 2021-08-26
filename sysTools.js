68015e7d992e722994fe402ea0709c7b92944599
elizhenin <elizhenin@gmail.com>
2021-08-26 15:58:58 +0300

    empty(bool) fix

--- sysTools.js
+++ sysTools.js
@@ -25,7 +25,7 @@ module.exports = function sysTools() {
                     break;
                 }
                 case 'boolean': {
-                    isEmpty = v;
+                    isEmpty = !v;
                     break;
                 }
                 case 'undefined': {
