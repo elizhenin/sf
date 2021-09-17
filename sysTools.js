daeda1e94b9a7b459c1204aec5038d9304caeb99
elizhenin <elizhenin@gmail.com>
2021-09-17 15:01:40 +0300

    base64 encode/decode fixed in browser

--- sysTools.js
+++ sysTools.js
@@ -1,7 +1,7 @@
 module.exports = function sysTools() {
     let Context = false;
-    if(typeof global != "undefined") Context = global;
-    if(typeof window != "undefined") Context = window;
+    if (typeof global != "undefined") Context = global;
+    if (typeof window != "undefined") Context = window;
     if (Context) {
 
 
@@ -49,22 +49,165 @@ module.exports = function sysTools() {
 
         Context.trim = function (string) {
             let result = ""
-            if (typeof string != "undefined") result = string.toString().trim();
+            if ("undefined" !== typeof string) result = string.toString().trim();
             return result
         }
 
         Context.explode = function (divider, string) {
             let result = []
-            if (typeof string != "undefined") result = string.toString().split(divider);
+            if ("undefined" !== typeof string) result = string.toString().split(divider);
             return result
         }
 
         Context.base64_encode = function (str) {
-            return new Buffer.from(str).toString('base64');
+            if ("undefined" !== typeof Buffer) {
+                return new Buffer.from(str).toString('base64');
+            } else {
+                /**
+                 *  part of
+                 *  Base64 encode / decode
+                 *  http://www.webtoolkit.info/
+                 *
+                 **/
+                let Base64 = {
+                    // private property
+                    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+                    // public method for encoding
+                    encode: function (input) {
+                        var output = "";
+                        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+                        var i = 0;
+
+                        input = Base64._utf8_encode(input);
+
+                        while (i < input.length) {
+
+                            chr1 = input.charCodeAt(i++);
+                            chr2 = input.charCodeAt(i++);
+                            chr3 = input.charCodeAt(i++);
+
+                            enc1 = chr1 >> 2;
+                            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                            enc4 = chr3 & 63;
+
+                            if (isNaN(chr2)) {
+                                enc3 = enc4 = 64;
+                            } else if (isNaN(chr3)) {
+                                enc4 = 64;
+                            }
+
+                            output = output +
+                                this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+                                this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+                        }
+                        return output;
+                    },
+                    // private method for UTF-8 encoding
+                    _utf8_encode: function (string) {
+                        string = string.replace(/\r\n/g, "\n");
+                        var utftext = "";
+
+                        for (var n = 0; n < string.length; n++) {
+
+                            var c = string.charCodeAt(n);
+
+                            if (c < 128) {
+                                utftext += String.fromCharCode(c);
+                            } else if ((c > 127) && (c < 2048)) {
+                                utftext += String.fromCharCode((c >> 6) | 192);
+                                utftext += String.fromCharCode((c & 63) | 128);
+                            } else {
+                                utftext += String.fromCharCode((c >> 12) | 224);
+                                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                                utftext += String.fromCharCode((c & 63) | 128);
+                            }
+                        }
+                        return utftext;
+                    }
+                }
+                return Base64.encode(str);
+            }
+
         };
 
         Context.base64_decode = function (str) {
-            return new Buffer.from(str, 'base64').toString();
+            if ("undefined" !== typeof Buffer) {
+                return new Buffer.from(str, 'base64').toString();
+            } else {
+                /**
+                 *  part of
+                 *  Base64 encode / decode
+                 *  http://www.webtoolkit.info/
+                 *
+                 **/
+                let Base64 = {
+
+                    // private property
+                    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+                    // public method for decoding
+                    decode: function (input) {
+                        var output = "";
+                        var chr1, chr2, chr3;
+                        var enc1, enc2, enc3, enc4;
+                        var i = 0;
+
+                        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+                        while (i < input.length) {
+
+                            enc1 = this._keyStr.indexOf(input.charAt(i++));
+                            enc2 = this._keyStr.indexOf(input.charAt(i++));
+                            enc3 = this._keyStr.indexOf(input.charAt(i++));
+                            enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+                            chr1 = (enc1 << 2) | (enc2 >> 4);
+                            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                            chr3 = ((enc3 & 3) << 6) | enc4;
+
+                            output = output + String.fromCharCode(chr1);
+
+                            if (enc3 != 64) {
+                                output = output + String.fromCharCode(chr2);
+                            }
+                            if (enc4 != 64) {
+                                output = output + String.fromCharCode(chr3);
+                            }
+                        }
+
+                        output = Base64._utf8_decode(output);
+
+                        return output;
+                    },
+                    // private method for UTF-8 decoding
+                    _utf8_decode: function (utftext) {
+                        var string = "";
+                        var i = 0;
+                        var c = c1 = c2 = 0;
+
+                        while (i < utftext.length) {
+
+                            c = utftext.charCodeAt(i);
+
+                            if (c < 128) {
+                                string += String.fromCharCode(c);
+                                i++;
+                            } else if ((c > 191) && (c < 224)) {
+                                c2 = utftext.charCodeAt(i + 1);
+                                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                                i += 2;
+                            } else {
+                                c2 = utftext.charCodeAt(i + 1);
+                                c3 = utftext.charCodeAt(i + 2);
+                                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                                i += 3;
+                            }
+                        }
+                        return string;
+                    }
+                }
+                return Base64.decode(str);
+            }
         };
 
         Context.md5 = function (str, as_bin = false) {
@@ -373,18 +516,18 @@ module.exports = function sysTools() {
         };
         Context.struct2flat = function (S) {
             let flat = {};
-    
+
             function resolver(obj, path = "", root = false) {
                 if (Array.isArray(obj)) {
-    
+
                     flat[path + '.length'] = obj.length;
                     for (let i = 0; i < obj.length; i++) {
                         let _path = path + '[' + i + ']';
-    
+
                         resolver(obj[i], _path);
                     }
                 } else {
-                    if (typeof obj == "object" && null!=obj) {
+                    if (typeof obj == "object" && null != obj) {
                         let keys = Object.keys(obj);
                         for (let i = 0; i < keys.length; i++) {
                             let _path = path + (root ? '' : '.') + keys[i];
@@ -394,12 +537,12 @@ module.exports = function sysTools() {
                         if (root) flat = obj;
                         else flat[path] = obj;
                     }
-    
+
                 }
             }
-    
+
             resolver(S, '', true);
-    
+
             return flat;
         }
         Context.list2tree = function (list) {
@@ -428,7 +571,7 @@ module.exports = function sysTools() {
             return struct2flat(tree);
         };
         //other
-        Context.GUID = function(){
+        Context.GUID = function () {
             //not as in RFC, but unique enough and correct in validators. JS is 53-bits integer timestamp and no MICROseconds available
             let guid = '';
             //prepare part using random
@@ -438,9 +581,11 @@ module.exports = function sysTools() {
                 return v.toString(16);
             });
             //prepare timestamp in hex in pattern 'hhhhhhhh-hhhh-'
-            let timestamp = (function(S){return `${S.slice(0,8)}-${S.slice(-4)}`})(('0'+(+Date.now()).toString(16)).slice(-12));
+            let timestamp = (function (S) {
+                return `${S.slice(0,8)}-${S.slice(-4)}`
+            })(('0' + (+Date.now()).toString(16)).slice(-12));
             //add timestamp to guid
-            guid = timestamp+guid;
+            guid = timestamp + guid;
             return guid;
         }
         /*
