a770028063379adb19b98005ba771f50435d696a
elizhenin <elizhenin@gmail.com>
2021-09-17 13:53:39 +0300

    fixed memory leak in Session

--- System/Session.js
+++ System/Session.js
@@ -4,18 +4,20 @@ try {
     if (tmp != undefined) _cookieName = tmp;
 } catch (e) {}
 
-let maxAge = 60 * 15; //15 minutes
-try {
-    if (!empty(Application.config.Session.CookieMaxAge)) maxAge = Application.config.Session.CookieMaxAge;
-} catch (e) {}
+let CookieMaxAge = 900; //15 minutes
+if (!empty(Application.config.Session.CookieMaxAge)) CookieMaxAge = Application.config.Session.CookieMaxAge;
+
 
 module.exports = {
     _cookieName: _cookieName,
     _storage: {},
+    _lastAccess: {},
     instance: class {
         constructor(session_id = '@') {
             if (empty(Application.System.Session._storage[session_id])) Application.System.Session._storage[session_id] = {};
             this._storage = Application.System.Session._storage[session_id];
+
+            Application.System.Session._lastAccess[session_id] = Date.now();
         }
 
         get(key, alt) {
@@ -38,8 +40,25 @@ module.exports = {
             session_id = req.cookies[Application.System.Session._cookieName];
         } else session_id = GUID();
 
-        res.setHeader('Set-Cookie', `${Application.System.Session._cookieName}=${session_id}; HttpOnly; Path=/;Max-Age=${maxAge}`);
+        res.setHeader('Set-Cookie', `${Application.System.Session._cookieName}=${session_id}; HttpOnly; Path=/;Max-Age=${CookieMaxAge}`);
+        Application.System.Session._lastAccess[session_id] = Date.now();
 
         return true;
     }
 }
+
+let CleanupInterval = 5000;
+if (!empty(Application.config.Session.CleanupInterval)) CleanupInterval = Application.config.Session.CleanupInterval;
+setInterval(function () {
+    let now = Date.now();
+    let end = CookieMaxAge * 1000;
+    let sessions = Object.keys(Application.System.Session._lastAccess);
+    let sessions_length = sessions.length;
+    for (let i = 0; i < sessions_length; i++) {
+        let age = now - Application.System.Session._lastAccess[session];
+        if (age > end) {
+            delete Application.System.Session._lastAccess[session];
+            delete Application.System.Session._storage[session];
+        }
+    }
+}, CleanupInterval)
