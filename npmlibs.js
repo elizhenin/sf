6080e118c9b42da06bde5ea153832c598380af65
Evgeny Lizhenin <elizhenin@gmail.com>
2023-03-14 21:25:04 +0300

    Node18

--- npmlibs.js
+++ npmlibs.js
@@ -3,7 +3,8 @@ Application.lib.path = require('path');
 Application.lib.fs = require('fs');
 Application.lib.fs_promises = require('fs').promises;
 Application.lib.nodemailer = require('nodemailer');
-Application.lib.request = require('request-promise-native');
+// Application.lib.request = require('request-promise-native'); //- no more. alt are "got" and "node-fetch"
+
 Application.lib.cyrillicToTranslit = new require("cyrillic-to-translit-js")();
 Application.lib.jsdom = require('jsdom');
 Application.lib.StrUnEscape = require('backslash');
